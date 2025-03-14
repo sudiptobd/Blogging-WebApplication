3388a29f26ea22a0aa8ad0b39f151312d8df281d
Sudipto Roy <sroy@miu.edu>
2025-03-14 16:09:16 -0500

    Main Express App

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import postsRoutes from "./routes/posts.js";
+import commentsRoutes from "./routes/comments.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/posts", postsRoutes);
+app.use("/posts/:id/comments", commentsRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
